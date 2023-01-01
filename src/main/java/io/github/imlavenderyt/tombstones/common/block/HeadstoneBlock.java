e44980c9998350d4b88fdf77faf18bf4aa28cf20
Theeskimo122 <52361845+Theeskimo122@users.noreply.github.com>
2023-01-01 14:40:58 -0500

    Made headstone water loggable and cleaned up code for better readability.

--- src/main/java/io/github/imlavenderyt/tombstones/common/block/HeadstoneBlock.java
+++ src/main/java/io/github/imlavenderyt/tombstones/common/block/HeadstoneBlock.java
@@ -1,28 +1,41 @@
 package io.github.imlavenderyt.tombstones.common.block;
 
 import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 
-public class HeadstoneBlock extends HorizontalFacingBlock {
-	public static final DirectionProperty FACING;
-	public static final VoxelShape FRONT_SHAPE;
-	public static final VoxelShape SIDE_SHAPE;
-	public static final VoxelShape DEBUG_SHAPE;
+public class HeadstoneBlock extends HorizontalFacingBlock implements Waterloggable {
+	public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+	public static final VoxelShape FRONT_SHAPE =
+			Block.createCuboidShape(1.0D, 0.0D, 6.0D, 15.0D, 16.0D, 10.0D);
+	public static final VoxelShape SIDE_SHAPE =
+			Block.createCuboidShape(6.0D, 0.0D, 1.0D, 10.0D, 16.0D, 15.0D);
+	public static final VoxelShape DEBUG_SHAPE =
+			Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
 
 
 	public HeadstoneBlock(Settings settings) {
 		super(settings);
-		this.setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH));
+		this.setDefaultState(this.getStateManager().getDefaultState()
+				.with(FACING, Direction.NORTH)
+				.with(WATERLOGGED, false));
 	}
 
 	public BlockState getPlacementState(ItemPlacementContext ctx) {
-		return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+		return this.getDefaultState()
+				.with(FACING, ctx.getPlayerFacing().getOpposite())
+				.with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).getFluid() == Fluids.WATER);
 	}
 
 	@Override
@@ -37,14 +50,19 @@ public class HeadstoneBlock extends HorizontalFacingBlock {
 	}
 
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-		builder.add(FACING);
+		builder.add(FACING, WATERLOGGED);
 	}
 
-	static {
-		FRONT_SHAPE = Block.createCuboidShape(1.0D, 0.0D, 6.0D, 15.0D, 16.0D, 10.0D);
-		SIDE_SHAPE = Block.createCuboidShape(6.0D, 0.0D, 1.0D, 10.0D, 16.0D, 15.0D);
-		DEBUG_SHAPE = Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-		FACING = HorizontalFacingBlock.FACING;
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+	}
+
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+		if (state.get(WATERLOGGED)) {
+			world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+		return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
 	}
 
 }
